f6e4b47851e86106dec82772e2c033ff2783664c
Pauline Pan <pan_pauline@yahoo.com>
2024-02-07 15:30:33 -0500

    Create search bar

--- src/features/Home.jsx
+++ src/features/Home.jsx
@@ -2,16 +2,26 @@ import "./Home.scss";
 
 export default function Home() {
   return (
-    <main className="home-main">
-      <section className="home-intro">
-        <h4>
-          "Welcome to the Nutrition Facts website. You can explore nutritional
-          information by either navigating to the 'Nutrition' tab or by
-          searching for specific foods using the search box. Simply enter the
-          name of the food, for example, 'broccoli', into the search box, and
-          you'll receive the nutritional facts directly on the same page."
-        </h4>
+    <>
+      <main className="home-main">
+        <section className="home-intro">
+          <h4>
+            "Welcome to the Nutrition Facts website. You can explore nutritional
+            information by either navigating to the 'Nutrition' tab or by
+            searching for specific foods using the search box. Simply enter the
+            name of the food, for example, 'broccoli', into the search box, and
+            you'll receive the nutritional facts directly on the same page."
+          </h4>
+        </section>
+      </main>
+      <section className="home-search">
+        <h1>Search: </h1>
+        <input
+          className="searchText"
+          type="text"
+          placeholder="Enter ingredient here..."
+        ></input>
       </section>
-    </main>
+    </>
   );
 }
